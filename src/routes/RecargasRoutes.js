2c5df0e7f89b2ccd2bb818eba800d0b6c3dfcb2c
Francisco <Francisco.jimenez.cohen@gmail.com>
2020-06-11 20:37:11 -0300

    Cambios en Rutas de recarga

--- src/routes/RecargasRoutes.js
+++ src/routes/RecargasRoutes.js
@@ -39,24 +39,25 @@ app.post('/Recargas', (req, res) => {
 
 //Realizamos la peticion del listado de Recargas
 app.get('/Recargas', (req, res) => {
-    // let body = req.body
-    // if (Object.keys(body).length === 0)
-    //     return res.status(403).json({
-    //         mensagge: 'error',
-    //         data: {
-    //             mensagge: 'Pcr is Required '
-    //         }
-    //     });
+    let body = req.query.nombreDelPunto
+    console.log(body);
+    if (body === '' || body === undefined)
+        return res.status(403).json({
+            mensagge: 'error',
+            data: {
+                mensagge: 'Pcr is Required '
+            }
+        });
 
-    // let arrayr = [];
-    // RECARGA.forEach(item => {
-    //     if (item.nombreDelPunto === body.nombreDelPunto)
-    //         arrayr.push(item)
-    // }
+    let arrayr = [];
+    RECARGA.forEach(item => {
+            if (item.nombreDelPunto === body)
+                arrayr.push(item)
+        }
 
 
-    // );
-    let arrayr = RECARGA;
+    );
+
 
 
     res.status(200).json({
