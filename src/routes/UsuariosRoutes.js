f99a130feaefab4ae6ba758adaadf0ab67eeabaf
Francisco <Francisco.jimenez.cohen@gmail.com>
2020-06-11 22:34:33 -0300

    Cambios en el pin Enoc

--- src/routes/UsuariosRoutes.js
+++ src/routes/UsuariosRoutes.js
@@ -1,10 +1,13 @@
+let Usuarios = require('../models/usuarioModel');
 const express = require('express');
+
 
 let app = express();
 
 app.get('/', (req, res) => {
     res.status(200).json({
-        menssage: 'hola'
+        menssage: 'true',
+        data: Usuarios
     });
 });
 
